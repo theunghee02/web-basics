918f52aed7cc0ef48aa0983d7443cdc95c5b7c92
theunghee02 <20211150@sungshin.ac.kr>
2021-04-08 14:02:52 +0900

    js comment

--- project/js/main.js
+++ project/js/main.js
@@ -5,6 +5,7 @@ const menuCloseEl = document.getElementById("btnClose")
 // 사이드 메뉴 선택자
 const menuNavEl = document.getElementById("navMenu")
 
+// addEventLister 클릭 반응
 menuOpenEl.addEventListener("click", function(){
     menuOpenEl.style.display = 'none'
     menuCloseEl.style.display = 'block'
